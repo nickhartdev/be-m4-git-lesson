0d436cfc9169832e0df09aa58c6971771f7c25a2
Nick Hart <nick.hart.des@gmail.com>
2020-10-06 10:48:39 -0600

    Comment out sessions_controller destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,10 +14,10 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy
-    session.clear
-    redirect_to root_path
-  end
+  # def destroy
+  #   session.clear
+  #   redirect_to root_path
+  # end
 
   private
 
